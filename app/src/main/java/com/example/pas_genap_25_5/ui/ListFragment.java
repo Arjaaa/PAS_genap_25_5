3e9c28d8cfe234be09b2e0dd524485a63ff82214
Arjaaa <dhitoarza@gmail.com>
2025-05-26 09:22:28 +0700

    Fragment

---
+++ app/src/main/java/com/example/pas_genap_25_5/ui/ListFragment.java
@@ -0,0 +1,5 @@
+package com.example.pas_genap_25_5.ui;
+import androidx.fragment.app.Fragment;
+
+public class ListFragment extends Fragment{
+}
---
+++ app/src/main/java/com/example/pas_genap_25_5/ui/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.pas_genap_25_5.ui;
+
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+
+import com.example.pas_genap_25_5.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frame_layout, new HomeFragment())
+                .commit();
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment fragment = null;
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.nav_home) {
+                fragment = new HomeFragment();
+            } else if (itemId == R.id.nav_list) {
+                fragment = new ListFragment();
+            } else if (itemId == R.id.nav_Profile) {
+                fragment = new ProfileFragment();
+            }
+
+            if (fragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.frame_layout, fragment)
+                        .commit();
+                return true;
+            }
+            return false;
+        });
+    }
+}
